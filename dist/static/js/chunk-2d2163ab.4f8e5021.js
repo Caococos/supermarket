1d47d4606fecea4419fa9b5a483cd6d8127093ac
Caococos <493524674@qq.com>
2021-02-22 22:49:43 +0800

    打包1

--- dist/static/js/chunk-2d2163ab.4f8e5021.js
+++ dist/static/js/chunk-2d2163ab.4f8e5021.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2163ab"],{c228:function(n,e,c){"use strict";c.r(e);var t=function(){var n=this,e=n.$createElement,c=n._self._c||e;return c("h2",[n._v("购物车")])},a=[],r={name:"Cart"},u=r,s=c("2877"),l=Object(s["a"])(u,t,a,!1,null,"2dccb7f7",null);e["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d2163ab.b1ff592e.js.map
+//# sourceMappingURL=chunk-2d2163ab.4f8e5021.js.map
